b02a378f743eee44912ba4073f648b26a2a21135
maxmib <30944505+maxmib@users.noreply.github.com>
2025-03-05 23:48:22 +0800

    Update index.vue

--- src/components/apps/About/index.vue
+++ src/components/apps/About/index.vue
@@ -1,33 +1,5 @@
 <script setup lang="ts">
-import { NDivider, NGradientText, NTag } from 'naive-ui'
-import { onMounted, ref } from 'vue'
-import { get } from '@/api/system/about'
-import { useAppStore } from '@/store'
 import srcSvglogo from '@/assets/logo.svg'
-import srcGitee from '@/assets/about_image/gitee.png'
-import srcGithub from '@/assets/about_image/github.png'
-import srcDocker from '@/assets/about_image/docker.png'
-import srcBilibili from '@/assets/about_image/bilibili.png'
-import srcYoutube from '@/assets/about_image/youtube.png'
-import srcQQGroupQR from '@/assets/about_image/qq_group_qr2.png'
-import { RoundCardModal } from '@/components/common'
-
-interface Version {
-  versionName: string
-  versionCode: number
-}
-
-const appStore = useAppStore()
-const versionName = ref('')
-const qqGroupQRShow = ref(false)
-const frontVersion = import.meta.env.VITE_APP_VERSION || 'unknown'
-
-onMounted(() => {
-  get<Version>().then((res) => {
-    if (res.code === 0)
-      versionName.value = res.data.versionName
-  })
-})
 </script>
 
 <template>
@@ -36,14 +8,6 @@ onMounted(() => {
       <img :src="srcSvglogo" width="100" height="100" alt="">
       <div class="text-3xl font-semibold">
         {{ $t('common.appName') }}
-      </div>
-      <div class="text-xl">
-        <NGradientText type="info">
-          <a class="font-semibold" :title="$t('apps.about.viewUpdateLog')" >v{{ versionName }}</a>
-        </NGradientText>
-      </div>
-      <div class="mt-2">
-        <a>{{ $t('apps.about.checkUpdate') }}</a>
       </div>
     </div>
   </div>
